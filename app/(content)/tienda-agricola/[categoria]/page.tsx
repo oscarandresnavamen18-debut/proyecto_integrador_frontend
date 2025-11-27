1d4e589f9258c604b1a8009848865f0b7f703699
jorgebotero190-cloud <jorgebotero190@gmail.com>
2025-11-27 06:44:03 -0500

    cambios de pagina tienda agricola

--- app/(content)/tienda-agricola/[categoria]/page.tsx
+++ app/(content)/tienda-agricola/[categoria]/page.tsx
@@ -14,7 +14,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function CategoriaPage({ params }: CategoriaPageProps) {
+async function CategoriaPage({ params }: CategoriaPageProps) {
   const { categoria } = await params;
   if (!categorias.includes(categoria as Categoria)) {
     notFound();
@@ -44,3 +44,4 @@ export async function generateMetadata({ params }: CategoriaPageProps) {
     description: info.descripcion,
   };
 }
+export default CategoriaPage;
